999e995a7a582634af04e2bc3566dfd51421c946
Meztli_RA <MeztliRA@gmail.com>
2020-12-13 08:27:55 +0700

    Added fish config folder.

---
+++ fish/config.fish
@@ -0,0 +1,14 @@
+# Fish config file
+
+### Aliases ###
+# ls aliases 
+alias ls="exa -l"
+alias la="exa -la"
+# cat aliases
+alias cat="bat"
+# grep aliases
+alias grep="rg"
+# ifetch aliases
+alias Ifetch="~/ifetch/Ifetch.sh"
+
+neofetch
